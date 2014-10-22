596f232f8996704023b8bbddcf79eab4b4a71379
mattkiernan <matt.kiernan@gmail.com>
2014-10-22 11:49:24 -0400

    Add delete button to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,6 +24,12 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_path 
+  end
+
   private
 
   def task_params
